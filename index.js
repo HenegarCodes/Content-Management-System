471ff7751d35a80633603c666e7ea3e16e852852
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-11-12 13:04:43 -0700

    adding questions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const tbale = require('console.table');
 
 
 
-var connection = mysql.createConnection({
+var connectionSQL = mysql.createConnection({
     host: "localhost",
     port: 3001,
     user: "root",
@@ -12,7 +12,25 @@ var connection = mysql.createConnection({
     database: "emptrack"
 });
 
-connection.connect(function (err) {
+connectionSQL.connect(function (err) {
     if(err) throw err;
-    console.log("connected as id " + connection)
+    console.log("connected as id " + connection.threadID + "\n");
+    tableQuestions();
 })
+
+function tableQuestions() {
+    inquirer.prompt({
+        message: "choice an action below:",
+        type: 'list',
+        choices: [
+            "view all employees",
+            "view all departments",
+            "add employee",
+            "add department",
+            "add role",
+            "update employee role",
+            "QUIT"
+        ]
+
+    })
+}
