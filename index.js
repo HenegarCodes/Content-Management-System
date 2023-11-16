7e1adf8fcb9803039f167baef681bf1e3110ecd3
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-11-15 17:21:26 -0700

    worked with tutor on sql to make sure correct format

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql');
-const tbale = require('console.table');
+const mysql2 = require("mysql2");
+const inquirer = require("inquirer");
+const table = require("console.table");
 
-
-
-var connectionSQL = mysql.createConnection({
-    host: "localhost",
-    port: 3001,
+var connection = mysql2.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
     user: "root",
-    password: "Leishy415!",
-    database: "emptrack"
+    password: "Baxter48",
+    database: "emp_db"
 });
 
-connectionSQL.connect(function (err) {
-    if(err) throw err;
-    console.log("connected as id " + connection.threadID + "\n");
-    tableQuestions();
-})
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    questionsPrompt();
+});
 
-function tableQuestions() {
+function questionsPrompt() {
     inquirer.prompt({
-        message: "choice an action below:",
-        type: 'list',
+        message: "what would you like to do?",
+        type: "list",
         choices: [
             "view all employees",
             "view all departments",
             "add employee",
-            "add department",
-            "add role",
-            "update employee role",
+            "add a department",
+            "add a role",
+            "update an employee role",
             "QUIT"
-        ]
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "view all employees":
+                employeeV()
+                break;
 
+            case "view all departments":
+                displayDepartments()
+                break;
+
+            case "add employee":
+                employeeAddition()
+                break;
+
+            case "add department":
+                addDepartment()
+                break;
+
+            case "add role":
+                roleAddition()
+                break;
+
+            case "update employee role":
+                employeeRoleUpdate();
+                break;
+
+            default:
+                connection.end()
+                break;
+        }
     })
 }
+
---
+++ sql/seeds.sql
@@ -0,0 +1,38 @@
+USE emp_db;
+
+INSERT INTO department (name)
+VALUES ("Salesman");
+INSERT INTO department (name)
+VALUES ("Engineer");
+INSERT INTO department (name)
+VALUES ("Analyst");
+INSERT INTO department (name)
+VALUES ("HR");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 74000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer", 89000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("IT Analyst", 100000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 185000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("HR rep", 300000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Andrew", "Patty", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Doug", "Prescott", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Rob", "Tracy", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Calvin", "Hardy", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Matt", "Micek", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lord", "Ring", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alicia", "Keys", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tuck", "Man", 1, 2);
